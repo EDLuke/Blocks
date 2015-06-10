aa5c1d76f765a7483fc5fbf0f366664997154dec
Luke Zhang <lukezhang213@gmail.com>
2015-06-09 20:27:14 -0400

    First commit, camera panning not really smooth, need to add user interface

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController: MonoBehaviour
+{
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 0.0001F;
+	public float sensitivityY = 0.0001F;
+	
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+	
+	public float minimumY = -360F;
+	public float maximumY = 360F;
+	
+    float rotationX = 0F;
+
+	public static float currentRotation;
+
+	public Vector2 mousePosStart;
+	public Vector2 mousePosEnd;
+
+	public bool drag;
+
+	void Update ()
+	{
+		//For Editor
+		if(!PlayerController.objectDrag){
+			if(Input.GetMouseButtonDown(0)){ //Drag starts
+				mousePosStart = Input.mousePosition;
+				drag = true;
+			}
+			if(Input.GetMouseButton(0) && drag){ //Dragging
+				mousePosEnd = Input.mousePosition;
+				rotationX = Input.GetAxis("Mouse X") * 15;
+				rotationX = Mathf.Clamp (rotationX, minimumY, maximumY);
+				transform.localEulerAngles = new Vector3(-rotationX, transform.localEulerAngles.y, 0);
+				transform.RotateAround(new Vector3(0, 0, 0), Vector3.up, rotationX);
+				transform.LookAt(Vector3.zero);
+
+				currentRotation += rotationX;
+				currentRotation %= 360;
+				//Debug.Log ("LOL " + currentRotation); 
+			}
+			if(Input.GetMouseButtonUp(0) && drag){ //Drag ends
+				drag = false;
+			}
+		}
+
+		//For in-game
+		if(!PlayerController.objectDrag){
+			if(Input.touches.Length > 0 && !drag){
+				mousePosStart = Input.touches[0].position;
+				drag = true;
+			}
+			if(Input.touches.Length > 0 && drag && Input.touches[0].phase == TouchPhase.Moved){
+				mousePosEnd = Input.touches[0].position;
+				print ("Moving " + mousePosEnd);
+				rotationX = (mousePosEnd.x > mousePosStart.x ? 1 : -1) * 15 * (Mathf.Abs(mousePosEnd.x - mousePosStart.x) / Screen.width);
+				print (rotationX);
+				rotationX = Mathf.Clamp (rotationX, minimumY, maximumY);
+				transform.localEulerAngles = new Vector3(-rotationX, transform.localEulerAngles.y, 0);
+				transform.RotateAround(new Vector3(0, 0, 0), Vector3.up, rotationX);
+				transform.LookAt(Vector3.zero);
+				
+				currentRotation += rotationX;
+				currentRotation %= 360;
+			}
+			if(Input.touches.Length <= 0){ //Drag ends
+				drag = false;
+			}
+
+		}
+
+//		int fingerCount = 0;
+//		foreach (Touch touch in Input.touches) {
+//			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled){
+//				print("User has " + touch.position);
+//				fingerCount++;
+//			}
+//			
+//		}
+//		if (fingerCount > 0)
+//			print("User has " + fingerCount + " finger(s) touching the screen");
+
+	}
+	
+	void Start ()
+	{
+		drag = false;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public Rigidbody rb;
+	public static bool objectDrag = false;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void FixedUpdate () {
+
+	}
+		
+
+	void OnMouseDown(){
+		//This grabs the position of the object in the world and turns it into the position on the screen
+		gameObjectSreenPoint = Camera.main.ViewportToWorldPoint(transform.position);
+		//Sets the mouse pointers vector3
+		mousePreviousLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, gameObjectSreenPoint.z);
+		objectDrag = true;
+	}
+
+	public Vector3 gameObjectSreenPoint;
+	public Vector3 mousePreviousLocation;
+	public Vector3 mouseCurLocation;
+	
+	public Vector3 force;
+	public Vector3 objectCurrentPosition;
+	public Vector3 objectTargetPosition;
+	public float topSpeed = 500;
+	void OnMouseDrag()
+	{
+		mouseCurLocation = new Vector3(Input.mousePosition.x, Input.mousePosition.y, gameObjectSreenPoint.z);
+		force = mouseCurLocation - mousePreviousLocation;//Changes the force to be applied
+		mousePreviousLocation = mouseCurLocation;
+
+		if (force.magnitude > topSpeed){
+			force = force.normalized * topSpeed;
+		}
+		//Debug.Log (CameraController.currentRotation);
+		force = Quaternion.AngleAxis(CameraController.currentRotation, Vector3.up) * force;
+		force.y *= 2;
+		rb.velocity = force;
+	}
+	
+	public void OnMouseUp()
+	{
+		objectDrag = false;
+	}
+
+//	void OnCollisionEnter(Collision collision) {
+//		rb.AddForce(-transform.forward * 500);
+//	}
+}
