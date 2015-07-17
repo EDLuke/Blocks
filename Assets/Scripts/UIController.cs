22b2e40bc18e862f12b425c7ea041ad9a8dbd4c1
Luke Zhang <lukezhang213@gmail.com>
2015-07-16 21:37:06 -0400

    Working on UI, need to do the transparent background for the buttons and the go-back functionality

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,21 +3,99 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class UIController : MonoBehaviour {
-	public GameObject sphere;
 	public GameObject cube;
-	public GameObject triangle;
 	public GameObject cone;
 	public GameObject cylinder;
-	public GameObject rectangle;
+	public GameObject triangleEquilateral;
+	public GameObject triangleAcute;
+	public GameObject triangleObtuse;
+	public GameObject triangleScalene;
+	public GameObject rectangleCube;
+	public GameObject rectangleFlat;
 
 	private GameObject selectedShape;
 	private Material selectedMaterial;
-	public GameObject panelColor;
+	private string selectedVariation;
+	private string selectedSize;
+
 	public GameObject panelShape;
+	public GameObject panelVariation;
+	public GameObject panelSize;
+	public GameObject panelColor;
+
+	private GameObject[] panels;
+
+	public Button btnBin;
 
 	// Use this for initialization
 	void Start () {
-		panelColor.SetActive (false);
+		panels = new GameObject[]{panelShape, panelVariation, panelSize, panelColor};
+		foreach (GameObject panel in panels) {
+			panel.SetActive(false);
+			assignBtnListen(panel);
+		}
+		btnBin.onClick.AddListener (delegate {
+			panelShape.SetActive (true);
+		});
+
+
+	}
+
+	private void assignBtnListen(GameObject currentPanel){
+		GameObject nextPanel = new GameObject ();
+		Button[] buttons = currentPanel.GetComponentsInChildren<Button> (true);
+		foreach (Button btn in buttons) {
+			
+			btn.onClick.AddListener(delegate {
+				switch (currentPanel.name.Substring (5)) {
+				case "Shape":
+					if(btn.name.Substring (3) == "RectangleCube" || btn.name.Substring (3) == "TriangleEquilateral"){
+						nextPanel = panelVariation;
+						movePanels(true);
+					}
+					else{
+						nextPanel = panelSize;
+						movePanels(false);
+					}
+					break;
+				case "Variation":
+					nextPanel = panelSize;
+					break;
+				case "Size":
+					nextPanel = panelColor;
+					break;
+				case "Color":
+					break;
+				}
+				nextPanel.SetActive(true);
+			});
+		}
+
+	}
+
+	private void movePanels(bool variation){
+		Vector3 currentSizePosition = panelSize.GetComponent<Transform> ().position;
+		Vector3 currentColorPosition = panelColor.GetComponent<Transform> ().position;
+		
+		Vector3 upSizePosition = currentSizePosition;
+		Vector3 downSizePosition = currentSizePosition;
+
+		Vector3 upColorPosition = currentColorPosition;
+		Vector3 downColorPosition = currentColorPosition;
+
+		upSizePosition.y += 50;
+		downSizePosition.y -= 50;
+
+		upColorPosition.y += 50;
+		downColorPosition.y -= 50;
+
+		if (variation) {
+			panelSize.transform.position = downSizePosition;
+			panelColor.transform.position = downColorPosition;		
+		} else {
+			panelSize.transform.position = upSizePosition;
+			panelColor.transform.position = upColorPosition;		
+		}
 	}
 
 
